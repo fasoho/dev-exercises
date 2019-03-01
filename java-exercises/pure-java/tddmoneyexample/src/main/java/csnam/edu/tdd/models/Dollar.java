d1028754e8ed868a07ad4cdf5deadf7ac7e51b8d
JP <1.4.gigz@gmail.com>
2019-03-01 17:53:43 -0600

    Make equals test pass. Closes #30.

--- java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/models/Dollar.java
+++ java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/models/Dollar.java
@@ -16,4 +16,9 @@ public class Dollar {
   public Dollar times(int multiplier) {
     return new Dollar(amount * multiplier);
   }
+
+  @Override
+  public boolean equals(Object object) {
+    return true;
+  }
 }
