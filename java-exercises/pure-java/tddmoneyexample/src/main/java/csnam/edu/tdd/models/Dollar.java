550c3827088eab4b9ce1b73aa07a464eb345aa5a
JP <1.4.gigz@gmail.com>
2019-03-01 16:21:48 -0600

    Refactor to remove duplication. Closes #26.

--- java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/models/Dollar.java
+++ java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/models/Dollar.java
@@ -7,11 +7,13 @@ package csnam.edu.tdd.models;
  */
 public class Dollar {
 
-  public int amount = 10;
+  public int amount;
 
   public Dollar(int amount) {
+    this.amount = amount;
   }
 
   public void times(int multiplier) {
+    amount *= multiplier;
   }
 }
