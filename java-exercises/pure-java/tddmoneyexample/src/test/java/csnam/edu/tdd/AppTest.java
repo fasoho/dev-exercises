82e390b698bf7b6f9e5db8d03f35811580bb2519
JP <1.4.gigz@gmail.com>
2019-03-01 17:50:03 -0600

    Write test to check for equality. Closes #29.

--- java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
@@ -1,6 +1,7 @@
 package csnam.edu.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import csnam.edu.tdd.models.Dollar;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,16 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 
   @Test
-  public void testMultiplication() {
+  public void testMultiplication() throws Exception {
     Dollar five = new Dollar(5);
     Dollar product = five.times(2);
     assertEquals(10, product.amount);
     product = five.times(3);
     assertEquals(15, product.amount);
+  }
+
+  @Test
+  public void testEquality() throws Exception {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
   }
 }
