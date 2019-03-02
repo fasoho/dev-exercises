06e45190b4ce2631a6a8214918ada5914c4e5dbd
JP <1.4.gigz@gmail.com>
2019-03-02 12:22:33 -0600

    Add dependencies and set up app structure. Closes #64.

--- java-exercises/pure-java/helloworldtdd/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/helloworldtdd/src/test/java/csnam/edu/tdd/AppTest.java
@@ -1,20 +1,9 @@
 package csnam.edu.tdd;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+
 }
