eced7146e0c2286d546bb17011c6712417829faf
JP <1.4.gigz@gmail.com>
2019-03-02 12:23:48 -0600

    Write test to check for greeting with no name. Closes #65.

--- java-exercises/pure-java/helloworldtdd/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/helloworldtdd/src/test/java/csnam/edu/tdd/AppTest.java
@@ -1,9 +1,16 @@
 package csnam.edu.tdd;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-
+  @Test
+  public void testHelloworldEmpty() throws Exception {
+    Greeting greeting = new Greeting();
+    assertEquals(greeting.getName(), "");
+    assertEquals(greeting.getMessage(), "Hello World");
+  }
 }
