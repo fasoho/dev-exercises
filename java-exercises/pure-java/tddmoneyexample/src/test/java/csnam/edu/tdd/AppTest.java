c97c2019a8ada69373f3acfe1f10eedfb6eda1d7
JP <1.4.gigz@gmail.com>
2019-03-01 18:29:07 -0600

    Update Franc equality test to compare Francs to Francs. Closes #38.

--- java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
@@ -31,5 +31,7 @@ public class AppTest {
   public void testEquality() throws Exception {
     assertTrue(new Dollar(5).equals(new Dollar(5)));
     assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
   }
 }
