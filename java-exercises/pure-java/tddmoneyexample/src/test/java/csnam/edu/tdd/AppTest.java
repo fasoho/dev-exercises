cb2834628838be531a8d21f4f368433759c2deb1
JP <1.4.gigz@gmail.com>
2019-03-01 20:17:53 -0600

    Write test to test Currencies. Closes #44.

--- java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
@@ -36,4 +36,10 @@ public class AppTest {
     assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
+
+  @Test
+  public void testCurrency() throws Exception {
+    assertEquals("USD", Money.dollar(1).currency());
+    assertEquals("CHF", Money.franc(1).currency());
+  }
 }
