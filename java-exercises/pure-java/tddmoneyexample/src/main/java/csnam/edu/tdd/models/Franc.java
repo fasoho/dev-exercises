606c502d573283fd102ca0b5904d2f5342770086
JP <1.4.gigz@gmail.com>
2019-03-01 18:30:37 -0600

    Remove Franc equality method. Closes #39.

--- java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/models/Franc.java
+++ java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/models/Franc.java
@@ -14,11 +14,4 @@ public class Franc extends Money {
   public Franc times(int multiplier) {
     return new Franc(amount * multiplier);
   }
-
-  @Override
-  public boolean equals(Object object) {
-    Franc dollar = (Franc) object;
-
-    return amount == dollar.amount;
-  }
 }
