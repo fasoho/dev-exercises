6f8e56f8ffe82abf1bfefe885c3c2c9eea1f72c5
JP <1.4.gigz@gmail.com>
2019-03-02 12:54:12 -0600

    Refactor to remove duplicate code. Closes #70.

--- java-exercises/pure-java/helloworldtdd/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/helloworldtdd/src/test/java/csnam/edu/tdd/AppTest.java
@@ -3,6 +3,7 @@ package csnam.edu.tdd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import csnam.edu.tdd.models.Greeting;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,16 +11,21 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 
+  private Greeting greeting;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    greeting = new Greeting();
+  }
+
   @Test
   public void testHelloworldEmpty() throws Exception {
-    Greeting greeting = new Greeting();
     assertEquals(greeting.getName(), "");
     assertEquals(greeting.getMessage(), "Hello World");
   }
 
   @Test
   public void testHelloworldWithName() {
-    Greeting greeting = new Greeting();
     greeting.setName("Tommy");
     assertEquals(greeting.getName(), "Tommy");
     assertEquals(greeting.getMessage(),"Hello Tommy");
