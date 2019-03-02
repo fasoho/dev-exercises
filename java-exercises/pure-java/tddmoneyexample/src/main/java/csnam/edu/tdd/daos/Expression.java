440bbb03e8597979095c31963fc108ca7967f61e
JP <1.4.gigz@gmail.com>
2019-03-02 10:31:01 -0600

    Add interface Expression to compile. Closes #51.

---
+++ java-exercises/pure-java/tddmoneyexample/src/main/java/csnam/edu/tdd/daos/Expression.java
@@ -0,0 +1,10 @@
+package csnam.edu.tdd.daos;
+
+/**
+ * @project tddmoneyexample
+ * @user jp
+ * @date 2019-03-02
+ */
+public interface Expression {
+
+}
--- java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
+++ java-exercises/pure-java/tddmoneyexample/src/test/java/csnam/edu/tdd/AppTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import csnam.edu.tdd.daos.Expression;
 import csnam.edu.tdd.models.Bank;
 import csnam.edu.tdd.models.Money;
 import org.junit.jupiter.api.Test;
